e46076529f20bb118a7fd70754a720ff253b9265
Maciej Pasternacki <maciej@3ofcoins.net>
2013-11-06 21:27:23 +0100

    Initial import

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             '3ofcoins'
+maintainer       'Maciej Pasternacki'
+maintainer_email 'maciej@3ofcoins.net'
+license          'MIT'
+description      'Make use of 3ofcoins.net resources'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
