690153b9d2fbeabd18bba255686152af19a5c191
Maciej Pasternacki <maciej@3ofcoins.net>
2013-11-06 23:15:47 +0100

    Use apt repo over http; we get GPG key by https, and apt gets confused by RapidSSL cert.

--- recipes/apt_repository.rb
+++ recipes/apt_repository.rb
@@ -1,7 +1,7 @@
 include_recipe 'apt'
 
 apt_repository '3ofcoins.net' do
-  uri "https://apt.3ofcoins.net/"
+  uri "http://apt.3ofcoins.net/"
   key "https://apt.3ofcoins.net/pubkey.gpg"
   distribution node['lsb']['codename']
   components ["main"]
